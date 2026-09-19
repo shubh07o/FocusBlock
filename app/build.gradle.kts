7f475c3ca47979b12e561ba1a667821ea7b588f2
shubh07o <rowdyji59@gmail.com>
2026-09-19 14:45:21 +0530

    chore: bump version to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.focusblock.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
